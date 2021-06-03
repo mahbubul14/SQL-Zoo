feab66b445a23a3988a2ca78821fd062ef9339cd
mahbubul14 <mahbub21alam2017@gmailcom>
2021-06-03 06:22:58 +0600

    Complete select from world questions

--- answers.sql
+++ answers.sql
@@ -1,4 +1,4 @@
-        -- SELECT basics
+      -- ###### SELECT basics ######
 
 --1. Introducing the world table of countries
 SELECT population FROM world
@@ -12,7 +12,7 @@ SELECT name, population FROM world
 SELECT name, area FROM world
   WHERE area BETWEEN 200000 AND 250000;
 
-        -- SELECT from WORLD Tutorial
+      -- ####### SELECT from WORLD Tutorial #######
 
 -- 1. Introduction
 SELECT name, continent, population FROM world;
@@ -38,3 +38,64 @@ SELECT name FROM world
 WHERE name like '%United%';
 
 -- 7. Two ways to be big
+SELECT name, population, area FROM world 
+WHERE area > 3000000 OR population >250000000;
+
+-- 8. One or the other (but not both)
+SELECT name, population, area FROM world
+  WHERE area > 3000000 AND population < 250000000 OR 
+  area < 3000000 AND population > 250000000;
+
+-- 9. Rounding
+SELECT name, ROUND(population/1000000,2), ROUND(gdp/1000000000,2) FROM world
+  WHERE continent = 'South America';
+
+-- 10. Trillion dollar economies
+SELECT name, ROUND(GDP/population,-3) FROM world
+  WHERE gdp > 1000000000000;
+
+-- 11. Name and capital have the same length
+SELECT name,capital FROM world
+ WHERE LEN(name) = LEN(capital)
+
+--  12. Matching name and capital
+SELECT name, capital FROM world
+WHERE LEFT(name,1) = LEFT(capital,1) and name <>capital
+
+-- 13. All the vowels
+SELECT name
+   FROM world
+WHERE name LIKE '%a%'
+  AND name LIKE '%e%' AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%' AND name NOT LIKE '% %';
+
+      -- ###### SELECT from Nobel Tutorial ######
+
+-- 1. Winners from 1950
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950
+
+-- 2. 1962 Literature
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature'
+
+-- 3. Albert Einstein
+SELECT yr, subject
+  FROM nobel
+ WHERE winner = 'Albert Einstein'
+
+-- 4. Recent Peace Prizes
+SELECT winner
+  FROM nobel
+ WHERE subject = 'Peace' AND yr >= 2000
+
+-- 5. Literature in the 1980's
+
+
+
+
+
+
+
