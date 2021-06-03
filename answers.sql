4d1e1d6d0ab0974c0c090435f2840cf95bd47848
mahbubul14 <mahbub21alam2017@gmailcom>
2021-06-03 08:43:53 +0600

    Complete sum and count answers

--- answers.sql
+++ answers.sql
@@ -1,4 +1,4 @@
-      -- ###### SELECT basics ######
+      -- ########## SELECT basics ##########
 
 --1. Introducing the world table of countries
 SELECT population FROM world
@@ -12,7 +12,7 @@ SELECT name, population FROM world
 SELECT name, area FROM world
   WHERE area BETWEEN 200000 AND 250000;
 
-      -- ####### SELECT from WORLD Tutorial #######
+      -- ########### SELECT from WORLD Tutorial #############
 
 -- 1. Introduction
 SELECT name, continent, population FROM world;
@@ -68,7 +68,7 @@ SELECT name
 WHERE name LIKE '%a%'
   AND name LIKE '%e%' AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%' AND name NOT LIKE '% %';
 
-      -- ###### SELECT from Nobel Tutorial ######
+      -- ########## SELECT from Nobel Tutorial ##########
 
 -- 1. Winners from 1950
 SELECT yr, subject, winner
@@ -92,6 +92,201 @@ SELECT winner
  WHERE subject = 'Peace' AND yr >= 2000
 
 -- 5. Literature in the 1980's
+
+
+-- 6.Only Presidents
+SELECT * FROM nobel
+ WHERE winner IN ('Theodore Roosevelt','Woodrow Wilson',
+                  'Jimmy Carter', 'Barack Obama');
+
+-- 7. John
+SELECT * FROM nobel
+ WHERE winner like 'John%';
+
+-- 8. Chemistry and Physics from different years
+SELECT * FROM nobel
+ WHERE yr = 1980 AND subject = 'Physics' OR yr = 1984 AND subject = 'Chemistry';
+
+-- 9. Exclude Chemists and Medics
+SELECT * FROM nobel
+ WHERE yr = 1980 AND subject NOT IN ('Medicine', 'Chemistry');
+
+-- 10. Early Medicine, Late Literature
+SELECT * FROM nobel
+ WHERE yr < 1910 AND subject IN ('Medicine') OR
+ yr >= 2004 AND subject IN ('Literature');
+
+-- 11. Harder Questions
+SELECT * FROM nobel
+ WHERE winner = 'PETER GRÜNBERG';
+
+-- 12. Apostrophe
+SELECT * FROM nobel
+ WHERE winner = 'EUGENE O''NEILL';
+
+-- 13. Knights of the realm
+SELECT winner, yr, subject FROM nobel
+ WHERE winner like 'Sir%' ORDER BY yr DESC, winner ASC;\
+
+-- 14. Chemistry and Physics last
+SELECT winner, subject
+  FROM nobel
+ WHERE yr = 1984
+ ORDER BY subject IN ('Physics','Chemistry'), subject, winner;
+
+      -- ############# SELECT within SELECT Tutorial ###########
+
+-- 1. Bigger than Russia
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia')
+
+-- 2. Richer than UK
+SELECT name FROM world
+  WHERE continent = 'Europe' AND 
+gdp/population > (SELECT gdp/population FROM world WHERE name='United Kingdom');
+
+-- 3. Neighbours of Argentina and Australia
+SELECT name, continent
+FROM world
+WHERE continent IN (SELECT continent
+  FROM world
+  WHERE name IN ('Argentina', 'Australia'))
+  ORDER BY name;
+
+-- 4. Between Canada and Poland
+SELECT name, population
+FROM world
+WHERE population > (SELECT population FROM world 
+  WHERE name = 'Canada') AND population < (SELECT population FROM world 
+    WHERE name = 'Poland');
+
+-- 5. Percentages of Germany
+SELECT name, CONCAT(ROUND(100*population/
+  (SELECT population FROM world WHERE name = 'Germany')), '%') AS percentage
+FROM world
+WHERE continent = 'Europe';
+
+-- 6. Bigger than every country in Europe
+SELECT name FROM world 
+  WHERE gdp > (SELECT MAX(gdp) FROM world 
+    WHERE continent = 'Europe');
+
+-- 7. Largest in each continent
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent
+          AND area>0);
+
+-- 8. First country of each continent (alphabetically)
+SELECT continent, name FROM world x
+  WHERE name <= ALL
+    (SELECT name FROM world y
+        WHERE y.continent=x.continent);
+
+-- 9. Difficult Questions That Utilize Techniques Not Covered In Prior Sections
+SELECT name, continent, population
+FROM world x
+WHERE 25000000 >= ALL(SELECT population FROM world y WHERE x.continent = y.continent);
+
+-- 10. 
+SELECT name, continent FROM world x
+  WHERE population >= ALL
+    (SELECT population * 3 FROM world y
+        WHERE y.continent=x.continent AND x.name <> y.name);
+
+-- ################## SUM and COUNT #####################
+
+-- 1. Total world population
+SELECT SUM(population)
+FROM world
+
+-- 2. List of continents
+SELECT DISTINCT continent
+FROM world;
+
+-- 3. GDP of Africa
+SELECT SUM(gdp)
+FROM world WHERE continent = 'Africa';
+
+-- 4. Count the big countries
+SELECT COUNT(area)
+FROM world WHERE area > 1000000;
+
+-- 5. Baltic states population
+SELECT SUM(population)
+FROM world WHERE name IN ('Estonia', 'Latvia', 'Lithuania');
+
+-- 6. Counting the countries of each continent
+SELECT continent, COUNT(name)
+FROM world
+GROUP BY continent;
+
+-- 7. Counting big countries in each continent
+SELECT continent, COUNT(name)
+FROM world
+WHERE population > 10000000
+GROUP BY continent;
+
+-- 8. Counting big continents
+
+SELECT continent
+FROM world
+GROUP BY continent
+HAVING SUM(population) > 100000000;
+
+
+
+
+
+
+
+
+
+
+
+-- 1.
+
+
+-- 2.
+
+
+-- 3.
+
+
+-- 4.
+
+
+-- 5.
+
+
+-- 6.
+
+
+-- 7.
+
+
+-- 8.
+
+
+-- 9.
+
+
+-- 10.
+
+
+-- 11.
+
+
+-- 12.
+
+
+-- 13.
+
+
+-- 14.
 
 
 
